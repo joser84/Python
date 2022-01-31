3828e5de8c950e9d2540837e12281e0da19cf95f
josesamuel.rojasrojas@gmail.com <samuelrr@digitalware.com.co>
2022-01-31 11:53:55 -0500

    update repository

---
+++ FlatFile/PrimerProyecto.py
@@ -0,0 +1,36 @@
+#import string
+#import numpy as np
+#import pandas as pd
+
+
+#cars = np.loadtxt('SURVEY-2020.csv',dtype=np.dtype,delimiter =';')
+
+
+with open('SURVEY-2020.csv','r') as rf:
+        with open('cars2.txt','w') as wf:
+            for line in rf:
+                wf.write(line)
+
+#with open('SURVEY-2020.csv','r') as f:
+
+ #   size_to_Read=10
+
+  #  f_contents=f.content
+
+
+ #   f_contents =f.read(10)
+  #  print(f_contents, end='')
+
+  #  for line in f:
+   #     print(line, end='')
+
+#    f_contents = f.readline()
+#    print (f_contents, end='')
+
+#f=open('SURVEY-2020.csv','r')
+#print(f.mode)
+#f.close()
+
+
+
+ 
